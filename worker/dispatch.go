194a8394befb25601702160a943ec152c284a53f
guest42069 <93949496+guest42069@users.noreply.github.com>
2021-12-01 01:26:02 +0000

    Update dispatch.go

--- worker/dispatch.go
+++ worker/dispatch.go
@@ -8,6 +8,7 @@ import (
 	"github.com/guest42069/i6r9c/msg"
 )
 
+// Worker will return 3 channels: output, input and a stop channel. Messages from the IRCd on the other side of conn are returned via the output channel, lines to send to the remote server should be sent to the input channel. closing the stop channel will stop the workers.
 func Worker(conn net.Conn) (<-chan *msg.Msg, chan<- string, chan bool) {
 	toCaller := make(chan *msg.Msg)
 	fromCaller := make(chan string)

39213f1cf64bdda4453a50b8dc55e7481e3a6eaf
guest42069 <93949496+guest42069@users.noreply.github.com>
2021-12-01 01:28:17 +0000

    Update nick.go

--- cmd/nick.go
+++ cmd/nick.go
@@ -8,6 +8,7 @@ import (
 	"github.com/docker/docker/pkg/namesgenerator"
 )
 
+// randomName returns to a pointer to a string containing a pseudo-random name.
 func randomName() *string {
 	rand.Seed(time.Now().Unix())
 	nick := strings.Replace(namesgenerator.GetRandomName(0), "_", "", 1)
